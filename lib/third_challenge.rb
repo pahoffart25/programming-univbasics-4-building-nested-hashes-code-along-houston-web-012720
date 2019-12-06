16e8171c0690ab96b676d41fcfdd6ae0c26139f4
Paul Hoffart <pahoffart25@gmail.com>
2019-12-06 21:08:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,8 +11,8 @@ def third_challenge
         :name => "Romeo", :age => '15', :status => "alive"
         },
       :hero_friends => [
-        {:name => "Benvolio", :age => "17", :attitude => 'worried'}
-        {:name => "Mercutio", :age => "18", :attitude = > 'hot-headed'}
+        {name: => "Benvolio", age: => "17", attitude: => 'worried'}
+        {name: => "Mercutio", age: => "18", attitude: = > 'hot-headed'}
       ]
    },
    :capulet => {
@@ -26,8 +26,8 @@ def third_challenge
         :name => 'Juliet', :age => "15", :status => "alive"
         },
       :heroine_friends => [
-        {:name => "Steven", :age => "30", :attitude => "confused" }
-        {:name => "nurse", :age => "44", :attitude => "worried"}
+        {name: => "Steven", age: => "30", attitude: => "confused" }
+        {name: => "nurse", age: => "44", attitude: => "worried"}
       ]
    }
   }
